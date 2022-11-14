caa79804c9fd6103ace5dcba11d557c3e6821570
Eryx <florrr1997@gmail.com>
2022-11-14 20:00:42 -0300

    Prueba pasar path completo para importar three js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css';
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module.js';
+// import * as THREE from three;
 import { gsap, Power1 } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
